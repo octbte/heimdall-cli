176bda231faf8d7d94834b11c567acb53d36372f
Hildebrando <hilde.pedroni@gmail.com>
2026-04-20 14:18:39 -0300

    chore: add release workflow and install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+REPO="octobit/heimdall-cli"
+BINARY="heimdall"
+INSTALL_DIR="/usr/local/bin"
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+case "$ARCH" in
+  x86_64) ARCH="amd64" ;;
+  arm64|aarch64) ARCH="arm64" ;;
+  *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+esac
+
+VERSION=$(curl -sf "https://api.github.com/repos/${REPO}/releases/latest" \
+  | grep '"tag_name"' \
+  | sed 's/.*"tag_name": *"\([^"]*\)".*/\1/')
+
+if [ -z "$VERSION" ]; then
+  echo "Could not determine latest version"; exit 1
+fi
+
+TARBALL="${BINARY}_${OS}_${ARCH}.tar.gz"
+URL="https://github.com/${REPO}/releases/download/${VERSION}/${TARBALL}"
+
+echo "Installing heimdall ${VERSION} (${OS}/${ARCH})..."
+curl -fsSL "$URL" | tar -xz "$BINARY"
+chmod +x "$BINARY"
+
+if [ -w "$INSTALL_DIR" ]; then
+  mv "$BINARY" "$INSTALL_DIR/$BINARY"
+else
+  sudo mv "$BINARY" "$INSTALL_DIR/$BINARY"
+fi
+
+echo "Done! Run: heimdall --help"
